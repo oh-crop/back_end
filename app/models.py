08a593f7073f30a92baa8abca91c422c050d76a6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-07-21 22:18:15 -0600

    Create plant model and test

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from flask import current_app, request, url_for
+from . import db
+
+class Plant(db.Model):
+    __tablename__ = 'plants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    plant_type = db.Column(db.String(50))
+    image = db.Column(db.Unicode(255))
+    lighting = db.Column(db.String(50))
+    water_frequency = db.Column(db.Integer)
+    harvest_time = db.Column(db.Integer, nullable=True)
+    root_depth = db.Column(db.Integer)
+    annual = db.Column(db.Boolean)

bed2752d31d93c6a1bbb8ee0f040586f946d6cb7
Kelsha_Darby <ohanaipo@gmail.com>
2020-07-24 14:25:31 -0600

    Create post request to add a plant to a garden - ROUGH DRAFT

--- app/api/routes.py
+++ app/api/routes.py
@@ -21,6 +21,32 @@ def get_plant(id):
     response.status_code = 200
     return response
 
+@api.route('/garden/plants', methods=['POST'])
+def add_to_garden(id):
+    plant_id = request.args['plant_id']
+    plant_name = request.args['plant_name']
+
+    plant = Plant.query.filter_by(id=plant_id).first()
+
+    garden = Garden(plant_id=plant_id)
+    garden.save()
+
+    days_to_harvest = (datetime.now() + timedelta(days=1))
+
+    response = jsonify({
+        'id': garden.id,
+        'plant_name': plant_name,
+        'last_watered': datetime.now(),
+        'plant_type': plant.plant_type,
+        'lighting': plant.lighting,
+        'root_depth_inches': plant.root_depth,
+        'days_between_water': plant.water_frequency,
+        'annual?': plant.annual,
+        'days_to_harvest':days_to_harvest
+    })
+    response.status_code = 200
+    return response
+
 @api.route('/plants/')
 def all_plants():
     plants = Plant.get_all()

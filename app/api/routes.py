873018b1755b65523ec669ea63ba4da6049df809
Kelsha_Darby <ohanaipo@gmail.com>
2020-07-24 00:51:42 -0600

    Create post request to add a plant to a garden - ROUGH DRAFT

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,12 @@ from . import api
 from ..models import Plant
 from flask_sqlalchemy import SQLAlchemy
 from app import db
+from datetime import datetime
+from datetime import timedelta
+
+some_engine = create_engine('postgresql://localhost/oh_crop')
+Session = sessionmaker(bind=some_engine)
+session = Session()
 
 
 @api.route('/')
@@ -18,6 +24,32 @@ def get_plant(id):
                 'plant_type': plant.plant_type
                 }
     response = jsonify(result)
+    response.status_code = 200
+    return response
+
+@api.route('/garden/plants', methods=['POST'])
+def add_to_garden(id):
+    plant_id = request.args['plant_id']
+    plant_name = request.args['plant_name']
+
+    plant = Plant.query.filter_by(id=plant_id).first()
+
+    garden = Garden(plant_id=plant_id)
+    garden.save()
+
+    days_to_harvest = (datetime.now() + timedelta(days=1))
+
+    response = jsonify({
+        'id': garden.id,
+        'plant_name': plant_name,
+        'last_watered': datetime.now(),
+        'plant_type': plant.plant_type,
+        'lighting': plant.lighting,
+        'root_depth_inches': plant.root_depth,
+        'days_between_water': plant.water_frequency,
+        'annual?': plant.annual,
+        'days_to_harvest':days_to_harvest
+    })
     response.status_code = 200
     return response
 

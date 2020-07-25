89ef7ff982d94a94fb97b5afeb71eff168e3b6df
Kelsha_Darby <ohanaipo@gmail.com>
2020-07-25 14:06:18 -0600

    Write garden endpoint testing

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -78,9 +78,21 @@ class EndpointTestCase(unittest.TestCase):
         self.assertIn("null", str(res.data))
 
     def test_api_can_return_all_plants_in_garden(self):
-        res = self.client().get('/api/v1/garden/1')
-        self.assertEqual(res.status_code, 200)
+        garden = Garden(id=1)
+        zeke = Plant(plant_type='Cherry Tomato',image='jim_photo.jpg',lighting='Full Sun',water_frequency=3,harvest_time=50,root_depth=12,annual="Annual")
+        dan = Plant(plant_type='Cactus',image='cactus_dan.jpg',lighting='Full Sun',water_frequency=7,harvest_time=None,root_depth=8,annual="Annual")
+        agatha = Plant(plant_type='Roma Tomato',image='agatha_photo.jpg',lighting='Full Sun',water_frequency=2,harvest_time=60,root_depth=12,annual="Annual")
+        db.session.add_all([zeke, dan, agatha, garden])
+        db.session.commit()
 
+        res = self.client().post('/api/v1/garden?plant_id={}&plant_name=Ezekiel'.format(zeke.id))
+        res = self.client().post('/api/v1/garden?plant_id={}&plant_name=Dan'.format(dan.id))
+
+        res = self.client().get('/api/v1/garden')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("Ezekiel", str(res.data))
+        self.assertIn("Dan", str(res.data))
+        self.assertNotIn("Agatha", str(res.data))
 
 # Execute test
 if __name__ == "__main__":
